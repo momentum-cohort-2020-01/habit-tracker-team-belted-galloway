da5ead8d0bf1e7a03f11a647cf6dba8a4cba7f2a
Quindallyn <qrk5002@gmail.com>
2020-03-05 14:59:59 -0500

    realize dont need user class so made habit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Habit(models.Model):
+    habit = models.CharField(max_length=500)
+    goal = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
