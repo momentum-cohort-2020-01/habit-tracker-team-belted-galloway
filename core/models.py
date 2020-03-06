35b227994ba3f4f905939ebc274b16b2a09086da
Quindallyn <qrk5002@gmail.com>
2020-03-06 15:47:00 -0500

    daily log model added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
@@ -12,7 +13,7 @@ class Habit(models.Model):
     goal_unit = models.ForeignKey('Unit', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(to=User, related_name='users_habits', on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, related_name='users_habits', on_delete=models.CASCADE, blank=True, null=True)
     
     def __str__(self):
         return f'Habit Name: {self.name} Daily Goal for: {self.goal}'
@@ -31,3 +32,12 @@ class Unit(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
+
+class DailyLog(models.Model):
+    activity_date = models.DateField(default=datetime.date.today)
+    habit = models.ForeignKey(to=Habit, related_name='habit_log', on_delete=models.CASCADE, blank=True ,null=True)
+    value = models.IntegerField(default=0)
+    comments = models.TextField(max_length=None)
+
+    def __str__(self):
+        return f'Date Tracked: {self.activity_date}, Habit: {self.habit.pk}'
